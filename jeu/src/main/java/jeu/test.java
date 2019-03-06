5ec88dde539327e12627f1ce0fdf41bfa09a6333
Reda066 <jalal.reda@outlook.com>
2019-03-06 12:43:38 +0100

    Correction test

--- jeu/src/main/java/jeu/test.java
+++ jeu/src/main/java/jeu/test.java
@@ -3,7 +3,7 @@ package jeu;
 public class test {
     public static void main (String[] args){
 
-        Carte c = new Carte(Age.I,Couleur.BLEU,5,"test",5);
+        Carte c = new Carte(Age.I,Couleur.BLEU,Materiaux.BOIS,5,"test",5);
         System.out.println(c.toString());
     }
 }
