946cf286b06766301b6bb7fde06720b85addd746
INabil06 <inesnabil06@gmail.com>
2019-03-27 17:00:15 +0100

    Ajout des cartes en fonction du nombre de joueur. close #32 and close #33

--- jeu/src/main/java/jeu/gestion/gestionCarte.java
+++ jeu/src/main/java/jeu/gestion/gestionCarte.java
@@ -18,41 +18,84 @@ public class gestionCarte {
 
     if (a.equals(Age.I)) {
 
+        if (nb_joueurs >= 3) {
+            //BLEU
+            cartes.add(new Carte(Age.I, Couleur.BLEU, "Autel", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.BLEU, "Bains", 3, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.BLEU, "Theatre", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
 
-        /*-------------------------------------------------------------------------BLEU----------------------------------------------------------------------------*/
-        cartes.add(new Carte(Age.I, Couleur.BLEU, "Autel", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.BLEU, "Bains", 3, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.BLEU, "Theatre", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        /*-------------------------------------------------------------------------ROUGE----------------------------------------------------------------------------*/
-        cartes.add(new Carte(Age.I, Couleur.ROUGE, "Caserne", 0, new Materiaux(0, 0, 0, 0, 1, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.ROUGE, "Palissade", 0, new Materiaux(0, 0, 1, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.ROUGE, "Tour de garde", 0, new Materiaux(0, 0, 0, 1, 0, 0, 0, 0)));
-        /*-------------------------------------------------------------------------JAUNE----------------------------------------------------------------------------*/
-        cartes.add(new Carte(Age.I, Couleur.JAUNE, "Comptoir Ouest", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.JAUNE, "Comptoir Est", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.JAUNE, "Marché", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            //ROUGE
+            cartes.add(new Carte(Age.I, Couleur.ROUGE, "Caserne", 0, new Materiaux(0, 0, 0, 0, 1, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.ROUGE, "Palissade", 0, new Materiaux(0, 0, 1, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.ROUGE, "Tour de garde", 0, new Materiaux(0, 0, 0, 1, 0, 0, 0, 0)));
 
-        //VERT
-        cartes.add(new Carte(Age.I, Couleur.VERT, "Officine", 0, new Materiaux(0, 0, 0, 0, 0, 1, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.VERT, "Scriptorium", 0, new Materiaux(0, 0, 0, 0, 0, 0, 1, 0)));
-        cartes.add(new Carte(Age.I, Couleur.VERT, "Atelier", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 1)));
+            //JAUNE
+            cartes.add(new Carte(Age.I, Couleur.JAUNE, "Comptoir Ouest", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.JAUNE, "Comptoir Est", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.JAUNE, "Marché", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
 
-        //MARRON
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Chantier", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Exploitation forestiere", 1, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Fosse argileuse", 3, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Bassin argileux", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Filon", 1, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.MARRON, "Cavite", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        // GRIS
-        cartes.add(new Carte(Age.I, Couleur.GRIS, "Verrerie", 5, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.GRIS, "Presse", 3, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
-        cartes.add(new Carte(Age.I, Couleur.GRIS, "Metier à tisser", 1, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            //VERT
+            cartes.add(new Carte(Age.I, Couleur.VERT, "Officine", 0, new Materiaux(0, 0, 0, 0, 0, 1, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.VERT, "Scriptorium", 0, new Materiaux(0, 0, 0, 0, 0, 0, 1, 0)));
+            cartes.add(new Carte(Age.I, Couleur.VERT, "Atelier", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 1)));
+
+            //MARRON
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Chantier", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Exploitation forestiere", 1, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Fosse argileuse", 3, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Bassin argileux", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Filon", 1, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.MARRON, "Cavite", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+
+            // GRIS
+            cartes.add(new Carte(Age.I, Couleur.GRIS, "Verrerie", 5, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.GRIS, "Presse", 3, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+            cartes.add(new Carte(Age.I, Couleur.GRIS, "Metier à tisser", 1, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
 
 
+        }
     }
+      if (nb_joueurs >= 4) {
 
-    //Prochaine itération
+          //BLEU
+          cartes.add(new Carte(Age.I, Couleur.BLEU, "Preteur sur gages", 3, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+
+          //ROUGE
+          cartes.add(new Carte(Age.I, Couleur.ROUGE, "Tour de garde", 0, new Materiaux(0, 0, 0, 1, 0, 0, 0, 0)));
+
+          //JAUNE
+          cartes.add(new Carte(Age.I, Couleur.JAUNE, "Taverne", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+
+          //VERT
+          cartes.add(new Carte(Age.I, Couleur.VERT, "Scriptorium", 0, new Materiaux(0, 0, 0, 0, 0, 0, 1, 0)));
+
+          //MARRON
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Chantier", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Filon", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Excavation", 0, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
+
+      }
+
+      if (nb_joueurs >= 5) {
+          //BLEU
+          cartes.add(new Carte(Age.I, Couleur.BLEU, "Autel", 2, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+          //ROUGE
+          cartes.add(new Carte(Age.I, Couleur.ROUGE, "Caserne", 0, new Materiaux(0, 0, 0, 0, 1, 0, 0, 0)));
+
+          //JAUNE
+          cartes.add(new Carte(Age.I, Couleur.JAUNE, "Taverne", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+
+          //VERT
+          cartes.add(new Carte(Age.I, Couleur.VERT, "Officine", 0, new Materiaux(0, 0, 0, 0, 0, 1, 0, 0)));
+
+          //MARRON
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Cavite", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Bassin argileux", 0, new Materiaux(0, 0, 0, 0, 0, 0, 0, 0)));
+          cartes.add(new Carte(Age.I, Couleur.MARRON, "Gisement", 0, new Materiaux(1, 0, 0, 0, 0, 0, 0, 0)));
+      }
+
+
+    //Prochaines itérations
     if (a.equals(Age.II)) {
 
     }
